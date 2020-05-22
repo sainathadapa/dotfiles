f03678049524f5c9ebe1f7c0eb4af5add599a29b
Sainath Adapa <adapasainath@gmail.com>
2020-05-22 11:33:56 +0200

    updated the scripts to work with different number of spaces per monitor

--- .config/yabai/scripts/cycle_projects_reverse.sh
+++ .config/yabai/scripts/cycle_projects_reverse.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
 currentSpace="$(yabai -m query --spaces --space | jq '.index')"
+# echo $currentSpace
 
-if [ $currentSpace -gt 6 ] && [ $currentSpace -le 10 ]
-then
-  leftSpace="$(($currentSpace - 1))"
-  rightSpace="$(($currentSpace - 6))"
-  yabai -m space --focus $rightSpace && yabai -m space --focus $leftSpace
+lastSpaceFirstMonitor="$((8))"
+lastSpaceSecondMonitor="$((16))"
+
+if [ $currentSpace -le $lastSpaceFirstMonitor ]; then
+    currentFirstMonitorSpace=$currentSpace
+    currentSecondMonitorSpace="$(($currentSpace + $lastSpaceSecondMonitor - $lastSpaceFirstMonitor))"
+else
+    currentFirstMonitorSpace="$(($currentSpace - $lastSpaceSecondMonitor + $lastSpaceFirstMonitor))"
+    currentSecondMonitorSpace=$currentSpace
 fi
+# echo $currentFirstMonitorSpace
+# echo $currentSecondMonitorSpace
 
-if [ $currentSpace -eq 6 ]
-then
-  leftSpace="$((10))"
-  rightSpace="$((5))"
-  yabai -m space --focus $rightSpace && yabai -m space --focus $leftSpace
+if [ $currentFirstMonitorSpace -le 1 ]; then
+    newFirstMonitorSpace="$((lastSpaceFirstMonitor))"
+else
+    newFirstMonitorSpace="$(($currentFirstMonitorSpace - 1))"
 fi
-
-if [ $currentSpace -gt 1 ] && [ $currentSpace -le 5 ]
-then
-  leftSpace="$(($currentSpace + 4))"
-  rightSpace="$(($currentSpace - 1))"
-  yabai -m space --focus $leftSpace && yabai -m space --focus $rightSpace
+firstSpaceSecondMonitor="$(($lastSpaceFirstMonitor + 1))"
+if [ $currentSecondMonitorSpace -le $firstSpaceSecondMonitor ]; then
+    newSecondMonitorSpace="$(($lastSpaceSecondMonitor))"
+else
+    newSecondMonitorSpace="$(($currentSecondMonitorSpace - 1))"
 fi
+# echo $newFirstMonitorSpace
+# echo $newSecondMonitorSpace
 
-if [ $currentSpace -eq 1 ]
-then
-  leftSpace="$((10))"
-  rightSpace="$((5))"
-  yabai -m space --focus $leftSpace && yabai -m space --focus $rightSpace
+if [ $currentSpace -le $lastSpaceFirstMonitor ]; then
+    yabai -m space --focus $newSecondMonitorSpace && yabai -m space --focus $newFirstMonitorSpace
+else
+    yabai -m space --focus $newFirstMonitorSpace && yabai -m space --focus $newSecondMonitorSpace
 fi
