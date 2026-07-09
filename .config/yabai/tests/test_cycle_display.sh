16889737b1b3964df317713892931fd2bdb42b59
Sainath Adapa <sainathadapa@users.noreply.github.com>
2026-07-09 11:04:04 -0400

    test: cover missing paired project space

--- .config/yabai/tests/test_cycle_display.sh
+++ .config/yabai/tests/test_cycle_display.sh
@@ -80,6 +80,22 @@ test_forward_two_display_pair() {
   assert_file_contains "$YABAI_COMMAND_LOG" "-m space --focus 3" "forward two-display cycle focuses paired first-display target"
 }
 
+test_forward_two_display_missing_pair_noops() {
+  reset_state
+  export YABAI_STUB_DISPLAYS='[{"index":1},{"index":2}]'
+  export YABAI_STUB_CURRENT_SPACE='{"index":2}'
+  export YABAI_STUB_SPACES='[{"index":1},{"index":2},{"index":3},{"index":9},{"index":10}]'
+
+  set +e
+  "$SCRIPT_DIR/cycle_projects.sh" 2> "$STDERR_LOG"
+  local status="$?"
+  set -e
+
+  assert_success "$status" "forward two-display missing paired target exits cleanly"
+  assert_file_contains "$STDERR_LOG" "space 11 does not exist" "forward two-display missing pair logs warning"
+  assert_file_not_contains "$YABAI_COMMAND_LOG" "--focus" "forward two-display missing pair does not focus spaces"
+}
+
 test_reverse_two_display_pair() {
   reset_state
   export YABAI_STUB_DISPLAYS='[{"index":1},{"index":2}]'
@@ -116,6 +132,7 @@ test_forward_one_display_wrap
 test_reverse_one_display_previous
 test_reverse_one_display_wrap
 test_forward_two_display_pair
+test_forward_two_display_missing_pair_noops
 test_reverse_two_display_pair
 test_three_display_cycle_logs_unsupported
 test_switch_one_display_noop
